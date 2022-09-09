f0edf7502b90f70c53d851f9c9b2aec7b3a612b6
gating <674964222@qq.com>
2022-09-09 18:20:16 +0800

    feat(components): 面包屑完善，添加父级路由index

--- src/permission.js
+++ src/permission.js
@@ -5,7 +5,6 @@ import router from './router'
 const whiteList = ['/login', '/401', '/404']
 
 router.beforeEach(async (to, from, next) => {
-  return next()
   if (includes(whiteList, to.path)) {
     return next()
   }
